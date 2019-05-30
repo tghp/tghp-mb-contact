0388bed9a2e606e85cedaaf52c567889598d354c
Josh Davenport <josh.davenport@tghp.co.uk>
2019-05-30 12:29:38 +0100

    Assign contact_submission_form taxonomy value using wp_set_object_terms instead of tax_input

--- inc/posts.php
+++ inc/posts.php
@@ -9,10 +9,6 @@ function tghpcontact_pre_process ($data, $config)
 
     $data['post_title'] = sprintf('%s - %s', $metaBox->title, date('d m Y'));
 
-    $data['tax_input'] = array(
-        'contact_submission_form' => array($metaBox->id)
-    );
-
     return $data;
 }
 add_filter('rwmb_frontend_insert_post_data', 'tghpcontact_pre_process', 10, 2);
@@ -22,9 +18,12 @@ add_filter('rwmb_frontend_insert_post_data', 'tghpcontact_pre_process', 10, 2);
  */
 function tghpcontact_after_process ($config, $postId)
 {
-    $id = $config['id'];
-    do_action('tghpcontact_after_process', $id, $postId);
-    do_action("tghpcontact_after_process_{$id}", $postId);
+    $metaBox = tghpcontact_get_contact_metabox($config['id']);
+
+    do_action('tghpcontact_after_process', $metaBox->id, $postId);
+    do_action("tghpcontact_after_process_{$metaBox->id}", $postId);
+
+    wp_set_object_terms($postId, [$metaBox->id], 'contact_submission_form');
 }
 add_filter('rwmb_frontend_after_process', 'tghpcontact_after_process', 10, 2);
 
