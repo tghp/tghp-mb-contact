757821946f4e49749e7f82b7c267e3eb6bb6bfc4
Josh Davenport-Smith <josh.davenport@tghp.co.uk>
2022-04-01 17:07:36 +0100

    Corrected metabox submission check for latest version of metabox

--- inc/validation/abstract.php
+++ inc/validation/abstract.php
@@ -8,7 +8,12 @@ class TGHPContact_Validator_Abstract {
 
     public static function getFieldValue($field) {
         $config = isset( $_POST['rwmb_form_config'] ) ? $_POST['rwmb_form_config'] : '';
+
         if (empty($config)) {
+            $key = $_POST['mbfs_key'];
+        }
+
+        if (empty($config) && empty($key)) {
             return;
         }
 
