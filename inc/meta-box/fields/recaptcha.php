f98cf38b69e4a5f5018cafb18ff7465c31bdc021
Jim Groome <james@jgroome.com>
2024-11-21 12:14:27 +0000

    Inject a different JS file if version 3 is specified

--- inc/meta-box/fields/recaptcha.php
+++ inc/meta-box/fields/recaptcha.php
@@ -9,16 +9,32 @@ if (class_exists('RWMB_Field') && !class_exists('RWMB_Recaptcha_Field')) {
         {
             parent::admin_enqueue_scripts();
             RWMB_File_Upload_Field::admin_enqueue_scripts();
-            wp_enqueue_script('tghpcontact-rwmb-recaptcha', TGHP_PLUGIN_URL . 'js/recaptcha.js', ['jquery'], TGHP_CONTACT_VERSION, true);
+            // wp_enqueue_script('tghpcontact-rwmb-recaptcha-v3', TGHP_PLUGIN_URL . 'js/recaptcha-v3.js', ['jquery'], TGHP_CONTACT_VERSION, true);
         }
 
         public static function html($meta, $field)
         {
+
             if (is_admin()) {
                 return '';
             }
 
             $randomId = substr(uniqid('', true), -5);
+
+            if ($field['version'] === 3) {
+
+                wp_enqueue_script('tghpcontact-rwmb-recaptcha-v3', TGHP_PLUGIN_URL . 'js/recaptcha-v3.js', ['jquery'], TGHP_CONTACT_VERSION, true);
+
+                return sprintf(
+                    '<div class="rwmb-recaptcha-v3" id="%1$s" data-key="%2$s" data-callback="tghpmbcontactOnRecaptchaSuccess%3$s" data-expired-callback="tghpmbcontactOnRecaptchaExpire%3$s"></div>%4$s',
+                    $field['id'],
+                    esc_attr($field['site_key']),
+                    $randomId,
+                    '<input type="hidden" name="recaptcha_fake" required>'
+                );
+            }
+
+            wp_enqueue_script('tghpcontact-rwmb-recaptcha', TGHP_PLUGIN_URL . 'js/recaptcha.js', ['jquery'], TGHP_CONTACT_VERSION, true);
 
             return sprintf(
                 '<div class="rwmb-recaptcha" id="%1$s" data-key="%2$s" data-callback="tghpmbcontactOnRecaptchaSuccess%3$s" data-expired-callback="tghpmbcontactOnRecaptchaExpire%3$s"></div>%4$s',
@@ -28,7 +44,5 @@ if (class_exists('RWMB_Field') && !class_exists('RWMB_Recaptcha_Field')) {
                 '<input type="hidden" name="recaptcha_fake" required>'
             );
         }
-
     }
-
 }
---
+++ js/recaptcha-v3.js
@@ -0,0 +1,50 @@
+jQuery(function ($) {
+  "use strict";
+
+  var recaptchaLoaded = false;
+
+  function loadRecaptcha(callback) {
+    if (recaptchaLoaded) {
+      callback();
+    } else {
+      window.tghpmbcontactOnRecaptchaLoad = function () {
+        recaptchaLoaded = true;
+        callback();
+      };
+
+      var scriptElement = document.createElement("script");
+      scriptElement.src =
+        "https://www.google.com/recaptcha/api.js?onload=tghpmbcontactOnRecaptchaLoad&render=explicit";
+      document.body.appendChild(scriptElement);
+    }
+  }
+
+  var $recaptcha = $(".rwmb-recaptcha-v3");
+
+  if ($recaptcha.length) {
+    loadRecaptcha(function () {
+      $recaptcha.each(function () {
+        var $this = $(this);
+
+        window[$this.data("callback")] = function (token) {
+          $this
+            .siblings('[name="recaptcha_fake"]')
+            .val(token)
+            .trigger("change");
+        };
+
+        window[$this.data("expired-callback")] = function () {
+          $this.siblings('[name="recaptcha_fake"]').val("").trigger("change");
+        };
+
+        $this.data(
+          "recaptcha",
+          grecaptcha.render($this.get(0), {
+            sitekey: $this.data("key"),
+            theme: "light",
+          })
+        );
+      });
+    });
+  }
+});
