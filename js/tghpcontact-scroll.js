5dbe40cea3ee93ed69cbcf84d8eefbe764456f97
Josh Davenport-Smith <josh.davenport@tghp.co.uk>
2026-02-16 14:41:00 +0000

    Allow disabling of auto scrolling to submission notification

--- js/tghpcontact-scroll.js
+++ js/tghpcontact-scroll.js
@@ -1,5 +1,9 @@
 (function ($) {
     $('body').on('tghpcontact:message-shown', function (e, messageElem) {
+        if (window.tghpcontactDisableScroll) {
+            return;
+        }
+
         if (messageElem) {
             if (messageElem.scrollIntoView) {
                 messageElem.scrollIntoView();
